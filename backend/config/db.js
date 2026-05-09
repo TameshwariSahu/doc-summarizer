db2b8082500dc93e3e7501fc9c8b11148cb141d9
Tameshwari <tameshwarisahu961@gmail.com>
2026-05-09 12:20:12 +0530

    made some changes in db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,20 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       tls: true,
+//       tlsAllowInvalidCertificates: true,
+//     });
+//     console.log('MongoDB connected!');
+//   } catch (err) {
+//     console.error('MongoDB connection failed:', err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
